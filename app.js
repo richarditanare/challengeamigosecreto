366cec6fb9b397825d5b9b1d121b97adf2279547
Richard Itanare <richardmig91@gmail.com>
2025-02-16 19:26:48 -0300

    falta implementar correctamente sortear amigo

--- app.js
+++ app.js
@@ -1,1 +1,56 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+console.log(amigos.length);
+    
+function agregarAmigo() {
+
+    let nombre = document.getElementById('amigo').value.trim(); //Captura el valor del texto
+
+    //Valida la entrada de datos
+    if (nombre === '') {
+        alert("Por favor, inserte un nombre."); 
+    } else if (amigos.includes(nombre)) {
+        alert("Este nombre ya está en la lista.");
+    } else {
+        amigos.push(nombre); //Se añade el nombre al array
+        console.log(amigos);
+        document.getElementById('amigo').value = ''; //Limpia el campo de texto
+        listaAmigos();
+    }
+}
+    
+function listaAmigos() {
+    console.log("Entre a la lista");
+
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ""; // Limpia la lista 
+
+     for (let i = 0; i < amigos.length; i++) { 
+         let nuevoNombre = document.createElement('li');
+         nuevoNombre.textContent ="-> " + amigos[i] + " <-";
+         lista.appendChild(nuevoNombre);
+     }
+}
+
+function sortearAmigo() {
+    if (amigos.length <= 2) {
+       alert("Introduzca al menos 3 nombres para jugar");
+    } else {
+        for (let i = 0; i < amigos.length; i++) {
+            const ganador = Math.random(amigos.length);
+            alert("El ganador es: " + ganador)
+        }
+    }
+ }
+
+function sotearAmigo() {
+    
+    console.log(amigos.length);
+    let posicion = Math.floor(Math.random()*amigos.length); //Tiene la posicion ganadora
+    let ganador = amigos[posicion];
+    alert("El ganador es: " + ganador)
+    borrarLista();
+}
+
+function borrarLista() {
+    
+}
